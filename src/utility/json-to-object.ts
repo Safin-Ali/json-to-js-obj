5c30ca17cb2ec4e20e297a5d6bdd47ef2b05f235
Safin Ali <108370873+Safin-Ali@users.noreply.github.com>
2023-09-22 17:36:49 +0600

    remove white space and print compile state

--- src/utility/json-to-object.ts
+++ src/utility/json-to-object.ts
@@ -2,10 +2,29 @@
  * Removes comments from a JSON string.
  *
  * @param {string} json - The input JSON string.
- * @returns {string} The JSON string with comments removed.
+ * @returns {object} The JSON string with comments removed.
  */
-const jsonPharse = (json:string) => {
-    return json.replace(/\\"|"(?:\\"|[^"])*"|(\/\/.*|\/\*[\s\S]*?\*\/)/g, (m, g) => g ? "" : m);
+const jsonPharse = (json: string): any => {
+	try {
+		
+		if (typeof json !== 'string') throw new Error('argument is not string');
+
+		// remove comments
+		json = json.replace(/\\"|"(?:\\"|[^"])*"|(\/\/.*|\/\*[\s\S]*?\*\/)\s/g, (m, g) => g ? "" : m);
+
+		// remove white space
+		json = json.replace(/\s/gm, '');
+
+		const jsObj = JSON.parse(json);
+
+		console.log(`\x1b[32m${'JSON to JS Compile Successfull'}\x1b[0m`);
+
+		// return clean js object
+		return jsObj;
+	}
+	catch (error: any) {
+		return console.log(`\x1b[31m${error.message}\x1b[0m`)
+	}
 };
 
 export default jsonPharse;
